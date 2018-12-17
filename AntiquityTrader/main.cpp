c7b64406a3180b5e0abac356d3cc38ae18d13548
Guillaume Collin <sledge@guiguibook-air.local>
2018-12-17 02:45:21 +0100

    Initial Commit

---
+++ AntiquityTrader/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AntiquityTrader
+//
+//  Created by Guillaume Collin on 17/12/2018.
+//  Copyright © 2018 Sledge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
